2b1d9517c835a60a61ad75589cc86ede167071e5
Joshua Quinn <comwhizy2k@hotmail.com>
2019-07-12 21:47:42 -0400

    3.3 - 3.5

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Josh";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
